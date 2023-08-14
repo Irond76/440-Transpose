3349e8cb98b3070f018f399337940e3eb2a0df17
Dale Bendixen <dalebendixen@gmail.com>
2023-08-14 08:36:53 -0400

    First Commit, Added Quick Styling

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<div class="key-card-container">
+  <div>
+    <button>click me</button>
+  </div>
+  <div>
+    <button>click me</button>
+  </div>
+  <div>
+    <button>click me</button>
+  </div>
+  <div>
+    <button>click me</button>
+  </div>
+  <div>
+    <button>click me</button>
+  </div>
+  <div>
+    <button>click me</button>
+  </div>
+</div>
